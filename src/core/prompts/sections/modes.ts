f27c80cdb0c7c00cc1dd03a739f8c2b2023078e1
skyer <yang.skyer@qq.com>
2025-05-09 19:52:12 +0800

    modes

--- src/core/prompts/sections/modes.ts
+++ src/core/prompts/sections/modes.ts
@@ -3,6 +3,7 @@ import * as vscode from "vscode"
 import { promises as fs } from "fs"
 
 import { ModeConfig, getAllModesWithPrompts } from "../../../shared/modes"
+import { compilePrompt } from "../template"
 
 export async function getModesSection(context: vscode.ExtensionContext): Promise<string> {
 	const settingsDir = path.join(context.globalStorageUri.fsPath, "settings")
@@ -10,20 +11,9 @@ export async function getModesSection(context: vscode.ExtensionContext): Promise
 
 	// Get all modes with their overrides from extension state
 	const allModes = await getAllModesWithPrompts(context)
+	const modes = allModes
+		.map((mode: ModeConfig) => `  * "${mode.name}" mode (${mode.slug}) - ${mode.roleDefinition.split(".")[0]}`)
+		.join("\n")
 
-	let modesContent = `====
-
-MODES
-
-- These are the currently available modes:
-${allModes.map((mode: ModeConfig) => `  * "${mode.name}" mode (${mode.slug}) - ${mode.roleDefinition.split(".")[0]}`).join("\n")}`
-
-	modesContent += `
-If the user asks you to create or edit a new mode for this project, you should read the instructions by using the fetch_instructions tool, like this:
-<fetch_instructions>
-<task>create_mode</task>
-</fetch_instructions>
-`
-
-	return modesContent
+	return await compilePrompt("sections/modes", { modes })
 }
---
+++ src/core/prompts/templates/en/sections/modes.md
@@ -0,0 +1,10 @@
+====
+
+MODES
+
+- These are the currently available modes:
+  {{modes}}
+  If the user asks you to create or edit a new mode for this project, you should read the instructions by using the fetch_instructions tool, like this:
+  <fetch_instructions>
+  <task>create_mode</task>
+  </fetch_instructions>
